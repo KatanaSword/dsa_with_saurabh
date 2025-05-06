3f183bd509d5ba33f740a449d69e0fd8edab76fe
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-05-07 01:32:46 +0530

    insert data at beginning and end

--- week00/linkedList/linkedList.js
+++ week00/linkedList/linkedList.js
@@ -18,3 +18,33 @@ class LinkedList {
     this.head = null;
   }
 }
+
+// Insert at the Beginning
+
+LinkedList.prototype.insertAtBeginning = function (data) {
+  // Convert into a node
+  const newNode = new Node(data);
+  // Add node to head
+  this.head = newNode;
+};
+
+// Insert at the End
+
+LinkedList.prototype.insertAtEnd = function (data) {
+  // Convert into a node
+  const newNode = new Node(data);
+  // Check If head is null
+  if (!this.head) {
+    // if true add new node
+    this.head = newNode;
+    return;
+  }
+  // If head not null
+  let last = this.head;
+  while (last.next) {
+    // If true move next
+    last = last.next;
+  }
+  // If false add new node
+  last.next = newNode;
+};
