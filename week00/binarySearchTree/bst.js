d1a46fa93963c3c8644f03589a893755d2964538
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-05-08 03:46:25 +0530

    insertion in bst

--- week00/binarySearchTree/bst.js
+++ week00/binarySearchTree/bst.js
@@ -10,4 +10,31 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  // Insertion
+
+  insert(key) {
+    const newNode = new BSTNode(key);
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) /* node -> root node */ {
+    if (newNode.key < node.key) {
+      if (!node.left) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (!node.right) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
 }
