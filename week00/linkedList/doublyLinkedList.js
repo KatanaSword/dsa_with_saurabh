ada6d3307bc7f881e08461a1c4d6650fca8cfbaf
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-05-07 08:13:29 +0530

    insert node at end

--- week00/linkedList/doublyLinkedList.js
+++ week00/linkedList/doublyLinkedList.js
@@ -20,12 +20,29 @@ doublyLinkedList.prototype.insertAtBeginning = function (data) {
   const newNode = new Node(data, null, this.head);
   // List is not empty
   if (this.head) {
-    this.head.prev = newNode; // point to new node nexNode prev
+    this.head.prev = newNode; // point to new node
     return;
   }
   this.head = newNode; // point head to a new node
   // tail is null
   if (this.tail === null) {
     this.tail = newNode; // point tail to a new node, there no other node to point
+  }
+};
+
+// Insert at the End
+
+doublyLinkedList.prototype.insertAtEnd = function (data) {
+  // Create a new node
+  const newNode = new Node(data, this.tail, null);
+  // List is not empty
+  if (this.tail) {
+    this.tail.next = newNode; // point to new node
+    return;
+  }
+  this.tail = newNode; // point tail to a new node
+  // head is null
+  if (this.head === null) {
+    this.head = newNode; // point head to a new node
   }
 };
