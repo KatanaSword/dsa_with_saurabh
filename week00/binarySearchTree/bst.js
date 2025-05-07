8b4cdb2b626af2ae283966f48c23aa36d4cc8667
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-05-08 04:48:20 +0530

    deletion of node in bst

--- week00/binarySearchTree/bst.js
+++ week00/binarySearchTree/bst.js
@@ -37,4 +37,44 @@ class BinarySearchTree {
       }
     }
   }
+
+  // Deletion of Node in BST
+
+  delete(key) {
+    this.root = this.deleteNode(this.root, key); // change root node every time function call
+  }
+
+  deleteNode(node, key) /* node -> root node */ {
+    if (node === null) {
+      return null;
+    }
+
+    if (key < node.key) {
+      node.left = this.deleteNode(node.left, key);
+    } else if (key > node.key) {
+      node.right = this.deleteNode(node.right, key);
+    } else {
+      if (node.left === null && node.right === null) {
+        return null; // remove root node and replace with null
+      } else if (node.left === null) {
+        return node.right; // remove root node and replace with right node
+      } else if (node.right === null) {
+        return node.left; // remove root node and replace with left node
+      } else {
+        let tempNode = this.findMinNode(node.right); // find node in right
+        node.key = tempNode.key; // replace root node with smallest node
+        node.right = this.deleteNode(node.right, tempNode.key); // remove smallest node
+      }
+    }
+    return node;
+  }
+
+  // Find smallest node
+
+  findMinNode(node) {
+    while (node.left) {
+      node = node.left;
+    }
+    return node;
+  }
 }
